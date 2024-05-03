857a1e1957e55d61ce45f34052961120a50f793a
Wanderson Lisboa <wandersonslisboa@gmail.com>
2024-05-03 11:09:53 -0300

    Configuração dos temas light, dark e system juntamente com a criação do header

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -3,7 +3,7 @@
 import { useTheme } from "next-themes";
 import React, { useEffect, useState } from "react";
 import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
-import { SunIcon } from "@radix-ui/react-icons";
+import { DesktopIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons";
 
 export default function ThemeSwitcher() {
   const { theme, setTheme } = useTheme();
@@ -16,10 +16,16 @@ export default function ThemeSwitcher() {
   if (!mounted) return null;
 
   return (
-    <Tabs defaultValue="{theme}">
+    <Tabs defaultValue={theme}>
       <TabsList className="border ">
         <TabsTrigger value="light" onClick={() => setTheme("light")}>
           <SunIcon className="h-[1.2rem] w-[1.2rem]" />
+        </TabsTrigger>
+        <TabsTrigger value="dark" onClick={() => setTheme("dark")}>
+          <MoonIcon className="h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0" />
+        </TabsTrigger>
+        <TabsTrigger value="sistem" onClick={() => setTheme("sistem")}>
+          <DesktopIcon className="h-[1.2rem] w-[1.2rem]" />
         </TabsTrigger>
       </TabsList>
     </Tabs>
